f2787ad0cb02479af1d00d072ca7976fb5550f07
Konrad Rodzik <konrad.rodzik@polidea.pl>
2013-02-22 19:24:29 +0100

    add description of code generation

--- main.cpp
+++ main.cpp
@@ -23,34 +23,26 @@
   }
   */
 
-class AView
-{
-};
-
-class ATextView : public AView
-{
-public:
-};
-
-class AButton : public ATextView
-{
-};
-
-class ALayoutView : public AView
-{
-public:
-    QString layout_width, layout_height;
-};
-
-class ALinearLayoutView : public ALayoutView
-{
-public:
-    QString orientation;
-};
-
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     
     return a.exec();
 }
+
+
+
+int aaaa() {
+
+    // TextView ->UILabel
+    // Parameters: objectName,parentName
+
+//    UILabel* %s = [UILabel alloc] initWithRect:CGRectZero];
+//    [%s setBackgroundColor:[UIColor clear]];
+//    [%s setColor:[UIColor redColor]];
+//    [%s addSubview:%s];
+
+
+
+}
+
