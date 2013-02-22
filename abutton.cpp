d8c9c2cd3aabd53c4302891323d6454a49ec9085
Błażej Marcinkiewicz <blazej.marcinkiewicz@polidea.pl>
2013-02-22 20:09:46 +0100

    Button fixing

--- abutton.cpp
+++ abutton.cpp
@@ -6,27 +6,27 @@ AButton::AButton()
 
 void AButton::write(QTextStream& writer, const QString& parentControlName)
 {
-    writer << varName() << "= [UIButton buttonWithType:UIButtonTypeCustom];\n";
-    writer << varName() << ".frame = CGRectMake(" << posX << ", " << posY << ", " << width << ", " << height << ");\n";
+    writer << varName() << "= [UIButton buttonWithType:UIButtonTypeCustom];" << endl;
+    writer << varName() << ".frame = CGRectMake(" << posX << ", " << posY << ", " << width << ", " << height << ");" << endl;
     if (text.length() > 0)
     {
-        writer << "[" << varName() << " setTitle:@\"" << text << "\" forState:UIControlStateNormal];\n";
+        writer << "[" << varName() << " setTitle:@\"" << text << "\" forState:UIControlStateNormal];" << endl;
     }
 
     if (imageName.length() > 0)
     {
-        writer << "[" << varName() << " setImage:[UIImage imageNamed:@\"" << imageName << "\"] forState:UIControlStateNormal];\n";
+        writer << "[" << varName() << " setImage:[UIImage imageNamed:@\"" << imageName << "\"] forState:UIControlStateNormal];" << endl;
     }
 
     if (backgroundImageName.length() > 0)
     {
-        writer << "[" << varName() << " setBackgroundImage:[UIImage imageNamed:@\"" << backgroundImageName << "\"] forState:UIControlStateNormal];\n";
+        writer << "[" << varName() << " setBackgroundImage:[UIImage imageNamed:@\"" << backgroundImageName << "\"] forState:UIControlStateNormal];" << endl;
     }
 
     if (onClickMethodName.length() > 0)
     {
-        writer << "[" << varName() << " addTarget:self action:@selector(" << onClickMethodName << ":) forControlEvents:UIControlEventTouchUpInside];\n";
+        writer << "[" << varName() << " addTarget:self action:@selector(" << onClickMethodName << ":) forControlEvents:UIControlEventTouchUpInside];" << endl;
     }
 
-    wrtier << "[" << parentControlName << " addSubview:" << varName() << "]\n";
+    wrtier << "[" << parentControlName << " addSubview:" << varName() << "];" << endl;
 }
