efb37ad800766dfd2b8397facd90021d25a14810
Błażej Marcinkiewicz <blazej.marcinkiewicz@polidea.pl>
2013-02-22 20:06:12 +0100

    Button output

--- abutton.cpp
+++ abutton.cpp
@@ -6,9 +6,27 @@ AButton::AButton()
 
 void AButton::write(QTextStream& writer, const QString& parentControlName)
 {
-    writer << varName() << "= [UIButton buttonWithType:UIButtonTypeCustom];\n"
-           << "[" << varName() << " setBackgroundColor:" << backgroundColor << "];\n"
-           << "[" << varName() << " setColor:" << color << "]\n"
-           << "[" << varName() << "setText:@\"" << text << "\"]\n"
-           << "[" << parentControlName << " addSubview:" << varName() << "]\n";
+    writer << varName() << "= [UIButton buttonWithType:UIButtonTypeCustom];\n";
+    writer << varName() << ".frame = CGRectMake(" << posX << ", " << posY << ", " << width << ", " << height << ");\n";
+    if (text.length() > 0)
+    {
+        writer << "[" << varName() << " setTitle:@\"" << text << "\" forState:UIControlStateNormal];\n";
+    }
+
+    if (imageName.length() > 0)
+    {
+        writer << "[" << varName() << " setImage:[UIImage imageNamed:@\"" << imageName << "\"] forState:UIControlStateNormal];\n";
+    }
+
+    if (backgroundImageName.length() > 0)
+    {
+        writer << "[" << varName() << " setBackgroundImage:[UIImage imageNamed:@\"" << backgroundImageName << "\"] forState:UIControlStateNormal];\n";
+    }
+
+    if (onClickMethodName.length() > 0)
+    {
+        writer << "[" << varName() << " addTarget:self action:@selector(" << onClickMethodName << ":) forEvent:UIEventTouchUpInside];\n";
+    }
+
+    wrtier << "[" << parentControlName << " addSubview:" << varName() << "]\n";
 }
