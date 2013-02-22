bbe5fb71721be388bbf65fa2db1aae859807a7ff
Błażej Marcinkiewicz <blazej.marcinkiewicz@polidea.pl>
2013-02-22 21:26:42 +0100

    Parsing parameters for textview and button

--- atextview.cpp
+++ atextview.cpp
@@ -6,11 +6,27 @@ ATextView::ATextView()
 
 void ATextView::write(QTextStream& writer, const QString& parentControlName)
 {
-    AView::write(writer, parentControlName);
-    writer << varName() << " = [UILabel alloc] initWithRect:CGRectZero];" << endl
-           << "[" << varName() << " setBackgroundColor:" << backgroundColor << "];" << endl
-           << "[" << varName() << " setColor:" << color << "];" << endl
-           << "[" << varName() << " setText:@\"" << text << "\"];" << endl
+    writer << varName() << " = [UILabel alloc] initWithRect:CGRectZero];" << endl;
+    if (backgroundColor.length() > 0) {
+        writer << "[" << varName() << " setBackgroundColor:" << backgroundColor << "];" << endl;
+    }
+
+    if (color.length() > 0) {
+        writer << "[" << varName() << " setColor:" << color << "];" << endl;
+    }
+    writer << "[" << varName() << " setText:@\"" << text << "\"];" << endl
            << "[" << parentControlName << " addSubview:" << varName() << "];" << endl
            << "[" << varName() << " setFrame:CGRectMake(" << posX << ", " << posY << ", " << width << ", " << height << ")];" << endl;
+}
+
+void ATextView::read(QDomNode &element)
+{
+    AView::read(element);
+    QDomElement e = element.toElement();
+
+    text = e.attribute("android:text");
+    posX = e.attribute("android:paddingLeft");
+    posY = e.attribute("android:paddingTop");
+    width = "0";
+    height = "0";
 }
